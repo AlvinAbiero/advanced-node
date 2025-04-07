c965b9f0709346fc945a66f8c985af74cec2069a
AlvinAbiero <abieroalvin@gmail.com>
2025-04-07 11:47:29 +0300

    feat: implemented socket to listen and send messages to clients

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const GithubStrategy = require("passport-github");
 const bcrypt = require("bcrypt");
 const { ObjectID } = require("mongodb");
 
@@ -29,5 +30,45 @@ module.exports = function (app, myDataBase) {
         return done(null, user);
       });
     })
+  );
+
+  passport.use(
+    new GithubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: "http://localhost:8080/auth/github/callback",
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        //Database logic here with callback containing our user object
+        myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              username: profile.username,
+              name: profile.displayName || "John Doe",
+              photo: profile.photos[0].value || "",
+              email: Array.isArray(profile.emails)
+                ? profile.emails[0].value
+                : "No public email",
+              created_on: new Date(),
+              provider: profile.provider || "",
+            },
+            $set: {
+              last_login: new Date(),
+            },
+            $inc: {
+              login_count: 1,
+            },
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            return cb(null, doc.value);
+          }
+        );
+      }
+    )
   );
 };
--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ module.exports = function (app, myDataBase) {
       message: "Please log in",
       showLogin: true,
       showRegistration: true,
+      showSocialAuth: true,
     });
   });
 
@@ -61,6 +62,21 @@ module.exports = function (app, myDataBase) {
       res.redirect("/profile");
     }
   );
+
+  app.route("/auth/github").get(passport.authenticate("github"));
+  app
+    .route("/auth/github/callback")
+    .get(
+      passport.authenticate("github", { failureRedirect: "/" }),
+      (req, res) => {
+        req.session.user_id = req.user.id;
+        res.redirect("/chat");
+      }
+    );
+
+  app.route("/chat").get(ensureAuthenticated, (req, res) => {
+    res.render("chat", { user: req.user });
+  });
 
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
